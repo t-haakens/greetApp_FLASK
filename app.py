9bb5440b4d13f6820707231f6edbfc1c94040d86
Torjus Haakens <64080136+t-haakens@users.noreply.github.com>
2022-07-02 21:44:45 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, flash
+
+# creates a class for our app
+app = Flask(__name__)
+
+app.secret_key = "manbearpig_123"
+
+# specify last part of url
+@app.route("/hello")
+def index():
+    flash("what's your name?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you")
+    return render_template("index.html")
+    
+
